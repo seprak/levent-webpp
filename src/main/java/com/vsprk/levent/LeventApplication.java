6597cb69477e0787efe57ce13674d256e718e7b7
Seprak <vladimir.seprak@allianz.com>
2023-10-09 11:47:32 +0200

    Initial commit of spring-boot app

---
+++ src/main/java/com/vsprk/levent/LeventApplication.java
@@ -0,0 +1,13 @@
+package com.vsprk.levent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsprk/levent/LeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsprk.levent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
